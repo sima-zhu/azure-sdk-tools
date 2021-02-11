deae0bbcbddd1488916578789ca345665954032e
Sima Zhu <sizhu@microsoft.com>
2021-02-11 10:25:34 -0800

    Search master in all places under repo

---
+++ find-main.ps1
@@ -0,0 +1,23 @@
+[CmdletBinding()]
+param(
+  [String] $Repo = "azure-sdk-tools"
+)
+
+git clone "github.com/azure/$($Repo)"
+cd $Repo
+
+$files = Get-ChildItem ./ -Recurse -File -Exclude *.map, *.min.js, bootstrap-grid.css, *.min.css, bootstrap.js, bootstrap.bundle.js, bootstrap-reboot.css
+
+foreach($file in $files) {
+    $contentRaw = Get-Content $file -Raw
+    if ($contentRaw -match "master") {
+        $content = Get-Content $file
+        $lineNum = 1
+        foreach ($line in $content) {
+            if ($line -match "master") {
+                Write-Host "$($file.FullName) : $lineNum"
+            }
+            $lineNum = $lineNum + 1
+        }
+    }
+}
