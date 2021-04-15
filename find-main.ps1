dd6687e4e289afdd198315727176651f6162422d
Sima Zhu <sizhu@microsoft.com>
2021-04-15 16:07:07 -0700

    Use literal path

--- find-main.ps1
+++ find-main.ps1
@@ -1,7 +1,7 @@
 $files = Get-ChildItem ./ -Recurse -File -Exclude *.map, *.min.js, bootstrap-grid.css, *.min.css, bootstrap.js, bootstrap.bundle.js, bootstrap-reboot.css
 $output = @()
 foreach($file in $files) {
-    $contentRaw = Get-Content $file -Raw
+    $contentRaw = Get-Content -LiteralPath $file -Raw
     if ($contentRaw -match "master") {
         $content = Get-Content $file
         $lineNum = 1
